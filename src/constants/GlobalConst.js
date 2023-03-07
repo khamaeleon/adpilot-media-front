1f990e2db0ef0b1fd9fff508d92f70b97119ba80
kspring <kspring@pounding.kr>
2023-03-07 18:50:27 +0900

    e5f99589065d41909af49905b10a45aa
    feat : 정산 정보 진행중

--- src/constants/GlobalConst.js
+++ src/constants/GlobalConst.js
@@ -4,13 +4,11 @@ const release = 'https://api.trycatch.co.kr';
 const media = 'http://map-vpn.iptime.org:8081/'
 const admin = 'http://map-vpn.iptime.org:8089 '
 const auth = 'http://map-vpn.iptime.org:8091'
-const dev = 'http://192.168.0.90:8081/'
-
 export const ADMIN_SERVER = admin
 
 export const AUTH_SERVER = auth
 
-export const MEDIA_SERVER = dev
+export const MEDIA_SERVER = media
 
 export const defaultImage = "https://static.trycatch.co.kr/assets/img/new-no-images.png";
 export const defaultSquareImage = "https://static.trycatch.co.kr/assets/img/new-no-image-square.png";
