d17435c1c2ce86b0fb6feb9f9777b918146229dc
chocto84 <chocto@findinglab.co.kr>
2023-03-14 10:14:41 +0900

    792720a2e44848d2ac06381260744549
    WARNING 제거 작업 몇개안남음

--- src/constants/GlobalConst.js
+++ src/constants/GlobalConst.js
@@ -3,14 +3,11 @@
 const media = 'http://192.168.0.102:8081'
 const admin = 'http://192.168.0.102:8089'
 const auth = 'http://192.168.0.102:8091'
-const exchange = 'http://192.168.0.25:8081'
 export const ADMIN_SERVER = admin
 
 export const AUTH_SERVER = auth
 
 export const MEDIA_SERVER = media
 
-
 /* admin@mcorpor.com /TestPassword!@34 */
-
 export const defaultImage = "https://static.trycatch.co.kr/assets/img/new-no-images.png";
