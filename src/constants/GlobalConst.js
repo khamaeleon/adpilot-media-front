2bdccdcbecf1e3e9326a49403ef3088b27855d1c
chocto84 <chocto@findinglab.co.kr>
2023-03-14 12:31:34 +0900

    포트 변경

--- src/constants/GlobalConst.js
+++ src/constants/GlobalConst.js
@@ -1,8 +1,8 @@
 
 // eslint-disable-next-line
-const media = 'http://192.168.0.102:8081'
-const admin = 'http://192.168.0.102:8089'
-const auth = 'http://192.168.0.102:8091'
+const media = 'http://192.168.0.102:8092'
+const admin = 'http://192.168.0.102:8091'
+const auth = 'http://192.168.0.102:8090'
 export const ADMIN_SERVER = admin
 
 export const AUTH_SERVER = auth
